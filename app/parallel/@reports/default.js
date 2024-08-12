cd9d3cfe87851568dd67636995901f328fe14c82
Oğuz Han Şahin <oguzhansahin95@outlook.com>
2024-08-12 15:35:55 +0300

    added description to default js

--- app/parallel/@reports/default.js
+++ app/parallel/@reports/default.js
@@ -1,3 +1,6 @@
+// @reports altında settings sayfası olmadığı için hard navigation esnasında default.js devreye girer.
+// default.js kullanılmama durumunda sayfa not found olarak gelecektir.
+
 export default async function ReportsDefaultPage() {
   return <div>Reports default page</div>;
 }
